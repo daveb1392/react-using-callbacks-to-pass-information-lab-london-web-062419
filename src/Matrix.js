017a9353558370a2e5c76d9ffad4bb623ced15d8
github username <github email address>
2019-09-01 12:46:49 +0100

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -4,32 +4,49 @@ import Cell from './Cell.js'
 import ColorSelector from './ColorSelector.js'
 
 export default class Matrix extends Component {
+                 constructor() {
+                   super();
+                   this.state = {
+                     selectedColor: "#FFF"
+                   };
+                 }
 
-  constructor() {
-    super()
-  }
+                 setSelectedColor = newColor => {
+                   this.setState({
+                     selectedColor: newColor
+                   });
+                 };
 
-  genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
-  )
+                 getSelectedColor = () => this.state.selectedColor;
+                 genRow = vals =>
+                   vals.map((val, idx) => (
+                     <Cell
+                       key={idx}
+                       color={val}
+                       getSelectedColor={this.getSelectedColor}
+                     />
+                   ));
 
-  genMatrix = () => (
-    this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
-  )
+                 genMatrix = () =>
+                   this.props.values.map((rowVals, idx) => (
+                     <div key={idx} className="row">
+                       {this.genRow(rowVals)}
+                     </div>
+                   ));
 
+                 // changeColor = ()
 
-  render() {
-    return (
-      <div id="app">
-        <ColorSelector />
-        <div id="matrix">
-          {this.genMatrix()}
-        </div>
-      </div>
-    )
-  }
-
-}
+                 render() {
+                   return (
+                     <div id="app">
+                       <ColorSelector
+                         setSelectedColor={this.setSelectedColor}
+                       />
+                       <div id="matrix">{this.genMatrix()}</div>
+                     </div>
+                   );
+                 }
+               }
 
 Matrix.defaultProps = {
   values: chromeBoi
